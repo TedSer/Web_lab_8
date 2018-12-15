59e40cc0755d416258bf1b35e17bc7a445e7688d
TedSer <32868217+TedSer@users.noreply.github.com>
2018-12-15 17:42:19 +0200

    Add files via upload

---
+++ 8лаб/js/news-script.js
@@ -0,0 +1,60 @@
+'use strict'
+
+Storage.prototype.setObj = function (key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function (key) {
+    return JSON.parse(this.getItem(key))
+}
+
+let storage = window.localStorage;
+let newsList = document.getElementById("newsList");
+window.addEventListener('online', checkLocalStorage);
+
+loadData()
+checkLocalStorage();
+
+function loadData() {
+    //loadDataFromServer
+}
+
+function isOnline() {
+    return window.navigator.onLine;
+}
+
+function checkLocalStorage() {
+
+    if (isOnline()) {
+        let localNews = storage.getObj("news");
+
+        if (localNews == null) {
+            storage.setObj("news", new Array());
+        }
+
+        if (localNews.length > 0) {
+            localNews.forEach(newsValue => {
+
+
+                newsValue = JSON.parse(newsValue);
+                let news = document.createElement('div');
+
+                news.className = "col-xl-3 col-md-6 col-sm-12";
+                news.innerHTML = `
+                <div class="card">
+                    <img src="https://cdn.britannica.com/81/198481-004-09C9E65F.jpg" alt="">
+                    <div class="card-content">
+                        <h5>${newsValue.titleInput}</h5>
+                        ${newsValue.newsInput}
+                    </div>
+                </div>`;
+                newsList.appendChild(news);
+
+
+
+            });
+
+            storage.setObj("news", new Array());
+        }
+
+    }
+}
